d7f4b4e6206f7dfff2ffe74ccf98117caf2761c5
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-03 06:58:29 +0530

    [Ganapati]Full Day Wage

--- empwage/Program.cs
+++ empwage/Program.cs
@@ -1,13 +1,8 @@
-﻿Console.WriteLine("WelCome to Employee Wage Computation");
-int present = 1;
-Random random = new Random();
-int status = random.Next(0, 2);
-Console.WriteLine("status=" + status);
-if (status == present)
-{
-    Console.WriteLine("Employee is Present");
-}
-else
-{
-    Console.WriteLine("Employee is Absent");
-}
+﻿int workinghrs = 8;
+int wageperhr = 20;
+Console.WriteLine("Employee is working Full day=8", workinghrs);
+Console.WriteLine("Wage per hr=20",wageperhr);
+int fulldaywage = workinghrs * wageperhr;
+Console.WriteLine(workinghrs * wageperhr );
+
+
