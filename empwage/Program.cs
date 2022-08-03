6a92d2367a2683a8cae4f015bcf63a5ee2fcaedb
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-03 06:06:40 +0530

    [Ganapati] Employee Attendance

---
+++ empwage/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("WelCome to Employee Wage Computation");
+int present = 1;
+Random random = new Random();
+int status = random.Next(0, 2);
+Console.WriteLine("status=" + status);
+if (status == present)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
