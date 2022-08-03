42a211680e9f1518cf0552511b6481b4666c7288
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-03 07:28:48 +0530

    [Ganapati]Added Part Time

--- empwage/Program.cs
+++ empwage/Program.cs
@@ -1,8 +1,27 @@
-﻿int workinghrs = 8;
+﻿int workinghrs = 0;
+int isparttime = 1;
+int isfulltime = 2;
 int wageperhr = 20;
-Console.WriteLine("Employee is working Full day=8", workinghrs);
-Console.WriteLine("Wage per hr=20",wageperhr);
-int fulldaywage = workinghrs * wageperhr;
-Console.WriteLine(workinghrs * wageperhr );
+
+Random random =new Random();
+int result = random.Next(0, 3);
+Console.WriteLine("result" + result);
+if (result == isparttime)
+{
+    workinghrs = 8;
+    Console.WriteLine("Part Workinghrs:" + workinghrs);
+    Console.WriteLine(workinghrs * wageperhr);
+}
+else if (result == isfulltime)
+{
+    workinghrs = 16;
+    Console.WriteLine("Full time Workinghrs:" + workinghrs);
+    Console.WriteLine(workinghrs * wageperhr);
+}
+else
+        { workinghrs = 0;
+    Console.WriteLine("No Working Hrs:" + workinghrs);
+    Console.WriteLine(workinghrs * wageperhr);
+}
 
 
