bbefd7fb93bd23119a2d74c23e7bd46e692905ba
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-03 18:35:35 +0530

    [Ganapati]Using Switch statement

--- empwage/Program.cs
+++ empwage/Program.cs
@@ -1,27 +1,31 @@
 ﻿int workinghrs = 0;
-int isparttime = 1;
-int isfulltime = 2;
+const int isparttime = 1;
+const int isfulltime = 2;
 int wageperhr = 20;
 
 Random random =new Random();
 int result = random.Next(0, 3);
 Console.WriteLine("result" + result);
-if (result == isparttime)
-{
-    workinghrs = 8;
-    Console.WriteLine("Part Workinghrs:" + workinghrs);
-    Console.WriteLine(workinghrs * wageperhr);
+switch (result)
+{ case isparttime:
+        workinghrs = 8;
+        Console.WriteLine("Working Hrs:" + workinghrs);
+        Console.WriteLine("Part Time");
+        break;
+    case isfulltime:
+        workinghrs = 16;
+        Console.WriteLine("Working Hrs:" + workinghrs);
+        Console.WriteLine("Full Time");
+        break;
+    default:
+        workinghrs = 0;
+        Console.WriteLine("Working Hrs:" + workinghrs);
+        Console.WriteLine("Absent");
+        break;
 }
-else if (result == isfulltime)
-{
-    workinghrs = 16;
-    Console.WriteLine("Full time Workinghrs:" + workinghrs);
-    Console.WriteLine(workinghrs * wageperhr);
-}
-else
-        { workinghrs = 0;
-    Console.WriteLine("No Working Hrs:" + workinghrs);
-    Console.WriteLine(workinghrs * wageperhr);
-}
+
+
+
+
 
 
