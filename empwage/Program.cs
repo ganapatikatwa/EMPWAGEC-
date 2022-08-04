16de302443e5f1cc08e50bd267c9f89a0bcc3999
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-04 06:57:29 +0530

    [Ganapati] Total Wages for 20 days

--- empwage/Program.cs
+++ empwage/Program.cs
@@ -1,27 +1,45 @@
-﻿int workinghrs = 0;
-const int isparttime = 1;
-const int isfulltime = 2;
-int wageperhr = 20;
+﻿class Program
+{
+    
+    public const int FULL_TIME = 1;
+    public const int PART_TIME = 2;
+    public const int EMP_RATE_PER_HR = 20;
+    public const int WORKING_DAYS = 20;
+    static void Main(string[] args)
+    {
+        
+        int EmpHours = 0;
+        int EmpWage = 0;
+        int TotalWage = 0;
 
-Random random =new Random();
-int result = random.Next(0, 3);
-Console.WriteLine("result" + result);
-switch (result)
-{ case isparttime:
-        workinghrs = 8;
-        Console.WriteLine("Working Hrs:" + workinghrs);
-        Console.WriteLine("Part Time");
-        break;
-    case isfulltime:
-        workinghrs = 16;
-        Console.WriteLine("Working Hrs:" + workinghrs);
-        Console.WriteLine("Full Time");
-        break;
-    default:
-        workinghrs = 0;
-        Console.WriteLine("Working Hrs:" + workinghrs);
-        Console.WriteLine("Absent");
-        break;
+        
+        Random random = new Random();
+
+        
+        for (int day = 1; day <= WORKING_DAYS; day++)
+        {
+            
+            int EmpInput = random.Next(0, 3);
+            
+            switch (EmpInput)
+            {
+                case FULL_TIME:
+                    EmpHours = 8;
+                    break;
+                case PART_TIME:
+                    EmpHours = 4;
+                    break;
+                default:
+                    EmpHours = 0;
+                    break;
+            }
+            
+            EmpWage = EMP_RATE_PER_HR * EmpHours;
+            TotalWage += EmpWage;
+        }
+        Console.WriteLine("Employee wage for " + WORKING_DAYS + "days = " + TotalWage);
+        Console.Read();
+    }
 }
 
 
