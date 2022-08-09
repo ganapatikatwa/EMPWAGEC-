b4321445dfcd8cf403a8f7626238b2e0b628c2d0
ganapatikatwa <108706896+ganapatikatwa@users.noreply.github.com>
2022-08-09 18:28:02 +0530

    Update Program.cs

--- empwage/Program.cs
+++ empwage/Program.cs
@@ -1,8 +1,20 @@
-﻿int workinghrs = 8;
+int workinghrs = 0;
+int fulltime= 1;
 int wageperhr = 20;
+Random random =new Random();
+int result = random.Next(0, 2);
+{
+if(result==1)
+{
+  workinghrs = 8;
 Console.WriteLine("Employee is working Full day=8", workinghrs);
 Console.WriteLine("Wage per hr=20",wageperhr);
 int fulldaywage = workinghrs * wageperhr;
 Console.WriteLine(workinghrs * wageperhr );
-
+}
+  else
+  {
+  Console.WriteLine("Employee is absent");
+  }
+}
 
