925341208a31f323da75f418ca5e3145ff37c4bd
[ganapat] <ganapatisakatwa@gmail.com>
2022-09-24 09:51:15 +0530

    [Ganapati]Refactor Using Class and Methods

--- empwage/Program.cs
+++ empwage/Program.cs
@@ -1,46 +1,14 @@
-﻿class Program
+﻿namespace empwage
 {
-    public const int FULL_TIME = 1;
-    public const int PART_TIME = 2;
-    public const int EMP_RATE_PER_HR = 20;
-    public const int MAX_WORKING_DAYS = 20;
-    public const int MAX_WORKING_HRS = 100;
-    static void Main(string[] args)
+    class Program
     {
-        int EmpHours = 0;
-        int EmpWage = 0;
-        int TotalWage = 0;
-        int WrorkingDays = 1;
-        int WorkingHrs = 0;
-
-        Random random = new Random();
-        while (WrorkingDays <= MAX_WORKING_DAYS && WorkingHrs <= MAX_WORKING_HRS)
+        public static void Main(String[] args)
         {
-            int EmpInput = random.Next(0, 3);
-            
-            switch (EmpInput)
-            {
-                case FULL_TIME:
-                    EmpHours = 8;
-                    break;
-                case PART_TIME:
-                    EmpHours = 4;
-                    break;
-                default:
-                    EmpHours = 0;
-                    break;
-            }
-            
-            EmpWage = EMP_RATE_PER_HR * EmpHours;
-            WorkingHrs += EmpHours;
-            TotalWage += EmpWage;
-            if (EmpInput != 0)
-            {
-                WrorkingDays++;
-            }
+            Console.WriteLine("Welcome to Employee Wage Computation Problem Part 3");
+            Console.WriteLine("Output of Total Employee wage for a Month  :");
+            EmloyeeWage emp = new EmloyeeWage();
+            emp.ComputeEmpWage();
+
         }
-        Console.WriteLine("WorkingHrs = " + WorkingHrs + " Working days =" + WrorkingDays);
-        Console.WriteLine("Employee wage for " + MAX_WORKING_DAYS + "days = " + TotalWage);
-        Console.Read();
     }
 }
