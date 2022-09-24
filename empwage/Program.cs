e3756f829937cb7c193c323e301994dbcc222d29
[ganapat] <ganapatisakatwa@gmail.com>
2022-09-24 11:21:14 +0530

    [Ganapati]Employee wage for Multiple Companies

--- empwage/Program.cs
+++ empwage/Program.cs
@@ -7,7 +7,8 @@
             Console.WriteLine("Welcome to Employee Wage Computation Problem Part 3");
             Console.WriteLine("Output of Total Employee wage for a Month  :");
             EmloyeeWage emp = new EmloyeeWage();
-            emp.ComputeEmpWage();
+            emp.ComputeEmpWage("Reliance", 20, 25, 150);
+            emp.ComputeEmpWage("D-Mart", 15, 20, 100);
 
         }
     }
